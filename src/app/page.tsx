69c495b2c30f3681ef825386d6873aef6db6bcef
Kevin <kevinpernia29@gmail.com>
2024-01-03 17:45:21 -0500

    ajustando ruta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { redirect } from 'next/navigation'
 
 export default function Home() {
 
-  redirect('/dashboard/main')
+  redirect('/dashboard/pokemons')
 
   // el redirect retorna never
   return (
